8de0440491181a63c6d0878f329c8aa5f5b12aeb
Nina Jhunjhnuwala <52934863+ninajhun@users.noreply.github.com>
2023-01-03 03:52:18 +0100

    Update exercise.js

--- let-and-const/exercise.js
+++ let-and-const/exercise.js
@@ -1,15 +1,8 @@
-// // Fix this code and use let and const to console.log all the items in the array 
-// var array = ["hi", "hello", "hey", "bye"];
+// Fix this code and use let and const to console.log all the items in the array 
+var array = ["hi", "hello", "hey", "bye"];
 
-// for (var i = 0; i < array.length; i++) {
-//   var message = array[i];
-// }
-// console.log(message);
-
-
-const array = ["hi", "hello", "hey", "bye"];
- for (let i = 0; i < array.length; i++) {
-    const message = array[i];
-    console.log(message);
- }
+for (var i = 0; i < array.length; i++) {
+  var message = array[i];
+}
+console.log(message);
 
